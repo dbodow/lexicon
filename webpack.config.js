fd57e9fa1197244a9968eb841a3e4ee8c828debb
David Bodow <dbodow@gmail.com>
2018-02-23 01:05:39 -0800

    Fixed an es2015 that should have been env for babel

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env']
         }
       }
     ]
